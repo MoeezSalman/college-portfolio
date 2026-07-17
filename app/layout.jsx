28a3f36d13d1010abfb4376afb95b49f882b2260
Muhammad Moeez <moeezsalman246@gmail.com>
2026-07-17 07:34:49 +0500

    made some changes in the layout component

--- app/layout.jsx
+++ app/layout.jsx
@@ -16,7 +16,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning data-scroll-behavior="smooth">
       <body
         className={`${inter.className} bg-white text-gray-900 dark:bg-slate-950 dark:text-white transition-colors duration-300`}
       >
